5f81e1a0c7e14454ada03f6a373ed88faa6ae366
Chris Beer <chris@cbeer.info>
2021-02-25 13:29:27 -0800

    Don't double-splat arguments that are supposed to be a hash upstream

--- app/controllers/blacklight/oembed/embed_controller.rb
+++ app/controllers/blacklight/oembed/embed_controller.rb
@@ -9,7 +9,7 @@ module Blacklight::Oembed
 
     ruby2_keywords def get_embed_content(url, add_params = {})
       begin
-        OEmbed::Providers.get(url, **add_params.transform_values { |v| CGI.escape(v) }).html.html_safe
+        OEmbed::Providers.get(url, add_params.transform_values { |v| CGI.escape(v) }).html.html_safe
       rescue OEmbed::NotFound
         response.status = 400
         ""
