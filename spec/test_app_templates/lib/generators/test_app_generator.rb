0ed6a79f6c617f899b078c76a14e9ea4497cc43a
Chris Beer <chris@cbeer.info>
2022-12-16 14:56:52 -0800

    Add CI for Blacklight 8.x

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -3,15 +3,21 @@ require 'rails/generators'
 class TestAppGenerator < Rails::Generators::Base
   source_root File.expand_path("../../../../test_app_templates", __FILE__)
 
-  def remove_index 
+  def remove_index
     remove_file "public/index.html"
   end
 
   def run_blacklight_generator
-    say_status("warning", "GENERATING BL", :yellow)       
+    say_status("warning", "GENERATING BL", :yellow)
+
+    if ENV['BLACKLIGHT_VERSION'] == 'github'
+      gem 'blacklight', github: 'projectblacklight/blacklight'
+    elsif ENV['BLACKLIGHT_VERSION'].present?
+      gem 'blacklight', ENV['BLACKLIGHT_VERSION']
+    end
 
     Bundler.with_clean_env do
-      run "bundle install"
+      run "bundle update"
     end
 
     generate 'blacklight:install'
