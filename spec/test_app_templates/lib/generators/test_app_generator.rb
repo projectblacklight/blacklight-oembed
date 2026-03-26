4258506776f96281c37981b0bc34d91ce284d29b
Justin Coyne <jcoyne@justincoyne.com>
2026-03-26 12:55:01 -0500

    with_clean_env has been removed in Bundler 4

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -1,14 +1,14 @@
 require 'rails/generators'
 
 class TestAppGenerator < Rails::Generators::Base
-  source_root File.expand_path("../../../../test_app_templates", __FILE__)
+  source_root File.expand_path('../../../test_app_templates', __dir__)
 
   def remove_index
-    remove_file "public/index.html"
+    remove_file 'public/index.html'
   end
 
   def run_blacklight_generator
-    say_status("warning", "GENERATING BL", :yellow)
+    say_status('warning', 'GENERATING BL', :yellow)
 
     if ENV['BLACKLIGHT_VERSION'] == 'github'
       gem 'blacklight', github: 'projectblacklight/blacklight'
@@ -16,8 +16,8 @@ class TestAppGenerator < Rails::Generators::Base
       gem 'blacklight', ENV['BLACKLIGHT_VERSION']
     end
 
-    Bundler.with_clean_env do
-      run "bundle update"
+    Bundler.with_unbundled_env do
+      run 'bundle update'
     end
 
     generate 'blacklight:install'
@@ -26,5 +26,4 @@ class TestAppGenerator < Rails::Generators::Base
   def run_oembed_install
     generate 'blacklight_oembed:install'
   end
-
 end
